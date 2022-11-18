24c8e70b50ea0f892a7b9baa2d19a867e54ac464
Võ Anh Quân <quancamilee@gmail.com>
2022-11-19 02:55:05 +0700

    Xong bài 2

--- app/src/main/java/com/hutech/exampractice/MainActivity.java
+++ app/src/main/java/com/hutech/exampractice/MainActivity.java
@@ -1,6 +1,11 @@
 package com.hutech.exampractice;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
 import android.support.design.widget.Snackbar;
@@ -13,6 +18,7 @@ import androidx.navigation.ui.NavigationUI;
 
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.FrameLayout;
 
 import com.hutech.exampractice.databinding.ActivityMainBinding;
 
@@ -20,6 +26,33 @@ public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
+    private BottomNavigationView bottomNavigationView;
+    private FrameLayout main_frame;
+    private BottomNavigationView.OnNavigationItemSelectedListener onNavigationItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+
+                    switch (menuItem.getItemId())
+                    {
+                        case R.id.nav_home:
+                            setFragement(new CategoryFragment());
+                            return true;
+
+                        case R.id.nav_leaderboard:
+                            setFragement(new LeaderBoardFragment());
+                            return true;
+
+                         case R.id.nav_account:
+                             setFragement(new AccountFragment());
+                             return true;
+
+                    }
+
+                    return false;
+                }
+            };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,37 +62,49 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.appBarMain.toolbar);
-        binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
+        bottomNavigationView = findViewById(R.id.bottom_nav_bar);
+        main_frame = findViewById(R.id.main_frame);
+        bottomNavigationView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener);
+        /*binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
                 .setDrawerLayout(drawer)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        /*NavController navController = Navigation.findNavController(this, R.id.nav_home);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
+        NavigationUI.setupWithNavController(navigationView, navController);*/
+
+        setFragement(new CategoryFragment());
+
+
     }
 
-    @Override
+    private void setFragement(Fragment fragement){
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(main_frame.getId(), fragement);
+        transaction.commit();
+    }
+    /*@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
-    }
+    }*/
 
-    @Override
+   /* @Override
     public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        NavController navController = Navigation.findNavController(this, R.id.nav_home);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
-    }
+    }*/
+
 }
--- app/src/main/java/com/hutech/exampractice/SplashActivity.java
+++ app/src/main/java/com/hutech/exampractice/SplashActivity.java
@@ -36,7 +36,9 @@ public class SplashActivity extends AppCompatActivity {
                 }
                 Intent intent = new Intent(SplashActivity.this, MainActivity.class);
                 startActivity(intent);
+                SplashActivity.this.finish();
+
             }
-        };
+        }.start();
     }
 }
