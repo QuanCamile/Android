a37e480dd8e1b3029730fb25f4318788ca2c68b8
Võ Anh Quân <quancamilee@gmail.com>
2022-11-18 23:08:02 +0700

    Xong bài 1

---
+++ app/src/main/java/com/hutech/exampractice/SplashActivity.java
@@ -0,0 +1,42 @@
+package com.hutech.exampractice;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.support.v4.content.res.ResourcesCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private TextView appName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        appName = findViewById(R.id.app_name);
+
+        Typeface typeface = ResourcesCompat.getFont(this, R.font.blacklist);
+        appName.setTypeface(typeface);
+
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.myanim);
+        appName.setAnimation(animation);
+
+        new Thread(){
+            @Override
+            public void run(){
+                try {
+                    sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        };
+    }
+}
