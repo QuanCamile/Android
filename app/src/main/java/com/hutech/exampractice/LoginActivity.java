f7269be75a58051129da0d89b3717f6fb369ff0f
Võ Anh Quân <quancamilee@gmail.com>
2022-11-19 18:58:37 +0700

    Xong bài 4

---
+++ app/src/main/java/com/hutech/exampractice/LoginActivity.java
@@ -0,0 +1,58 @@
+package com.hutech.exampractice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+    private EditText email, pass;
+    private Button loginB;
+    private TextView forgotPassB, signupB;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        email = findViewById(R.id.email);
+        pass = findViewById(R.id.password);
+        loginB = findViewById(R.id.LoginB);
+        forgotPassB = findViewById(R.id.forgot_password);
+        signupB = findViewById(R.id.signupB);
+
+        loginB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(validateData())
+                {
+                    login();
+                }
+            }
+        });
+    }
+
+    private boolean validateData(){
+
+
+        if(email.getText().toString().isEmpty())
+        {
+            email.setError("Enter E-Mail ID");
+            return false;
+        }
+
+        if(pass.getText().toString().isEmpty())
+        {
+            pass.setError("Enter Password");
+            return false;
+        }
+
+        return true;
+    }
+
+    private void login(){
+
+    }
+}
